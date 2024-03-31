18bf360cbe6bc5271da2393e68a04a20c525182b
M Salma <muhsalaa@gmail.com>
2024-03-31 09:40:23 +0700

    feat: add carousel

---
+++ src/lib/components/__/Carousel.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import clsx from 'clsx';
+	import ButtonIcon from '$lib/components/_/ButtonIcon.svelte';
+	import { Icon, ChevronRight, ChevronLeft } from 'svelte-hero-icons';
+
+	export let items: string[] = ['/images/no-pict.jpg'];
+
+	const LENGTH = 576;
+	let wrapper: HTMLDivElement;
+	let index = 0;
+
+	function next() {
+		if (index + 1 < items.length) {
+			wrapper.scrollTo({
+				left: LENGTH * (index + 1),
+				behavior: 'smooth'
+			});
+			index += 1;
+		}
+	}
+
+	function prev() {
+		if (index - 1 > -1) {
+			wrapper.scrollTo({
+				left: LENGTH * (index - 1),
+				behavior: 'smooth'
+			});
+			index -= 1;
+		}
+	}
+</script>
+
+<div class="relative">
+	{#if items.length > 1}
+		<ButtonIcon
+			class={clsx(
+				'absolute right-4 top-1/2 -translate-y-1/2 bg-gray-200/50 h-12 w-12',
+				index === items.length - 1 && 'hidden'
+			)}
+			on:click={next}><Icon src={ChevronRight} /></ButtonIcon
+		>
+		<ButtonIcon
+			class={clsx(
+				'absolute left-4 top-1/2 -translate-y-1/2 bg-gray-200/50 h-12 w-12',
+				index === 0 && 'hidden'
+			)}
+			on:click={prev}><Icon src={ChevronLeft} /></ButtonIcon
+		>
+	{/if}
+	<div class="snap-x flex snap-mandatory w-full overflow-x-auto no-scrollbar" bind:this={wrapper}>
+		{#each items as item, rowIndex (rowIndex)}
+			<img class="snap-start aspect-video" src={item} alt={`banner ${rowIndex}`} />
+		{/each}
+	</div>
+	{#if items.length > 1}
+		<div class="absolute bottom-2 gap-2 flex left-1/2 -translate-x-1/2">
+			{#each items as _, rowIndex (rowIndex)}
+				<div
+					class={clsx(
+						'w-2.5 h-2.5 rounded-full',
+						index === rowIndex ? 'bg-gray-700' : 'bg-gray-200'
+					)}
+				/>
+			{/each}
+		</div>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import Button from '$lib/components/_/ButtonIcon.svelte';
+	import Carousel from '$lib/components/__/Carousel.svelte';
+
+	const images = [
+		'https://lelogama.go-jek.com/post_featured_image/jaringan-prima-promo_blog-header_720.jpg',
+		'https://d2v6npc8wmnkqk.cloudfront.net/storage/11331/conversions/Header-Blog-GoPay-Promo-Game-seo_image_large.jpg',
+		'https://news.codashop.com/id/wp-content/uploads/sites/4/2022/10/Gopay_Blog.jpg',
+		'https://foto.kontan.co.id/2CosVjMLjJGrGXt7bOqoAkXrAP0=/smart/2021/12/09/1533324503p.jpg'
+	];
 </script>
 
 <div>
-	<img
-		src="https://d2v6npc8wmnkqk.cloudfront.net/storage/11331/conversions/Header-Blog-GoPay-Promo-Game-seo_image_large.jpg"
-		class="h-auto w-full"
-		alt="banner"
-	/>
+	<Carousel items={images} />
 </div>
